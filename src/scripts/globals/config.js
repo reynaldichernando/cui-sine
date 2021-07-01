a169729dd2d82addd137c2c69c706111dc07b337
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-07-02 00:26:42 +0700

    Add cache API

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -3,6 +3,8 @@ const CONFIG = {
   BASE_IMAGE_SMALL_URL: 'https://restaurant-api.dicoding.dev/images/small/',
   BASE_IMAGE_MEDIUM_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_LARGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
 };
 
 export default CONFIG;
