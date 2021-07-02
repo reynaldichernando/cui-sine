42c878f8f1d3f468dc501a3f0fb1e9f8c1ddcffc
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-07-03 00:00:12 +0700

    Add loader for API fetch

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -6,15 +6,19 @@ import LikeButtonInitiator from '../../utils/like-button-initiator';
 const Detail = {
   async render() {
     return `
+      <div class="loader">Loading...</div>
       <div id="restaurant" class="restaurant"></div>
       <div id="likeButtonContainer"></div>
     `;
   },
   async afterRender() {
     const url = UrlParser.parseActiveUrlWithoutCombiner();
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'block';
     const restaurant = await RestaurantSource.detailRestaurant(url.id);
     const restaurantContainer = document.querySelector('#restaurant');
     restaurantContainer.innerHTML = createRestaurantDetailTemplate(restaurant);
+    loader.style.display = 'none';
 
     LikeButtonInitiator.init({
       likeButtonContainer: document.querySelector('#likeButtonContainer'),
--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -6,17 +6,21 @@ const Home = {
     return `
     <div class="content">
       <h2 class="content__heading">Explore Restaurants</h2>
+      <div class="loader">Loading...</div>
       <div id="restaurant-list" class="restaurants">
       </div>
     </div>
     `;
   },
   async afterRender() {
+    const loader = document.querySelector('.loader');
+    loader.style.display = 'block';
     const restaurants = await RestaurantSource.restaurantList();
     const restaurantContainer = document.querySelector('#restaurant-list');
     restaurants.forEach((restaurant) => {
       restaurantContainer.innerHTML += createRestaurantItemTemplate(restaurant);
     });
+    loader.style.display = 'none';
   },
 };
 
