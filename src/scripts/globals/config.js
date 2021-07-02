fda819591c1641c07bc3f49547c5586921e6749a
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-07-02 23:36:10 +0700

    Update cache name

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   BASE_IMAGE_MEDIUM_URL: 'https://restaurant-api.dicoding.dev/images/medium/',
   BASE_IMAGE_LARGE_URL: 'https://restaurant-api.dicoding.dev/images/large/',
   DEFAULT_LANGUAGE: 'en-us',
-  CACHE_NAME: new Date().toISOString(),
+  CACHE_NAME: 'cuisine-v1',
   DATABASE_NAME: 'restaurants-database',
   DATABASE_VERSION: 1,
   OBJECT_STORE_NAME: 'restaurants',
