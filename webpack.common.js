d35d72a7e9cc3c7d9ba9e775f7412fec88147cad
Reynaldi Chernando <reynaldichernando@gmail.com>
2021-07-21 00:12:23 +0700

    Move bundle analyzer to dev

--- webpack.common.js
+++ webpack.common.js
@@ -4,13 +4,12 @@ const path = require('path');
 const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
 const ImageminWebpackPlugin = require('imagemin-webpack-plugin').default;
 const ImageminMozjpeg = require('imagemin-mozjpeg');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/scripts/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
   },
   module: {
     rules: [
@@ -54,6 +53,5 @@ module.exports = {
         }),
       ],
     }),
-    new BundleAnalyzerPlugin(),
   ],
 };
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,14 @@
 const { merge } = require('webpack-merge');
 const path = require('path');
 const common = require('./webpack.common');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new BundleAnalyzerPlugin(),
+  ]
 });
